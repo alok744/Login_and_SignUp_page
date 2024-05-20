abe97edb1c30fccf33ccf9c59cb0b62c7f1edab0
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-20 17:32:06 +0530

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,111 @@
+﻿using Login_Signup_page.Migrations;
+using Login_Signup_page.Models;
+using Login_Signup_page.Models.Account;
+using Login_Signup_page.Models.Account.ViewModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Login_Signup_page.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        private Login_Dbcontext db;
+
+        public AccountController(Login_Dbcontext DB)
+        {
+            db = DB;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User1 model)
+        {
+            if(ModelState.IsValid)
+            {
+                var data=db.LoginSignUpModel.Where(e => e.UserName == model.UserName).SingleOrDefault();
+
+                db.Add(new User1
+                {
+                    UserName=model.UserName,
+                    Password=model.Password,
+                    IsRemember=model.IsRemember
+                });
+                db.SaveChanges();
+                if (data !=null)
+                {
+                    bool isValid=(data.UserName==model.UserName && data.Password==model.Password);
+                    if(isValid)
+                    {
+                        var identity=new ClaimsIdentity(new[] {new Claim(ClaimTypes.Name, model.UserName) },CookieAuthenticationDefaults.AuthenticationScheme);
+                        var principal=new ClaimsPrincipal(identity);
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                        HttpContext.Session.SetString("UserName", model.UserName);                     
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    else
+                    {
+                        TempData["errorMessage"] = "Invalid Password";
+                        return View(model);
+                    }
+                }
+                else
+                {
+                    TempData["errorMessage"] = "User data not found";
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+       
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(LoginSignUpModel model)
+        {
+            if (model!=null)
+            {
+                db.Add(new LoginSignUpModel
+                {
+                    UserName= model.UserName,
+                    Email= model.Email,
+                    Mobile=model.Mobile,
+                    Password=model.Password,
+                    Confirm_Password=model.Confirm_Password,
+                    IsActive=model.IsActive,
+                    IsRemember=model.IsRemember,
+                });
+                db.SaveChanges();
+                TempData["sucessMessage"] = "Data Added sucessfully";
+            }
+            else
+            {
+                TempData["errorMessage"] = "Data is Null";
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using Login_Signup_page.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login_Signup_page.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Login_Dbcontext db;
+
+        public EmployeeController(Login_Dbcontext DB)
+        {
+            db = DB;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Employee_get()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Employee_get(Employee employee)
+        {
+            if (employee!= null)
+            {
+               db.Add(new Employee
+                {
+                    Employee_name = employee.Employee_name,
+                    Employee_email = employee.Employee_email,
+                    Employee_phone = employee.Employee_phone,
+                    Employee_salary = employee.Employee_salary,
+                    Employee_status = employee.Employee_status,
+
+                });
+                db.SaveChanges();
+                TempData["sucessMessage"] = "Data Inserted Sucessfully";
+            }
+            else
+            {
+                TempData["errorMessage"] = "InValid Data";
+            }
+            
+            return View();
+        }
+        public IActionResult Employee_update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Employee_update(int id, Employee employee)
+        {
+           
+            if (id != null)
+            {
+               db.Employee.Update(employee);
+               var result = db.SaveChanges();
+               TempData["sucessMessage"] = "Data added sucessfully";
+             
+            }
+            else
+            {
+                TempData["errorMessage"] = "Data is invalid";
+            }
+            return View();
+        }
+
+        public IActionResult Employee_delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Employee_delete(int id, Employee employee)
+        {
+            if (id != null)
+            {
+                db.Employee.Remove(employee);
+                var result=db.SaveChanges();
+                TempData["sucessMessage"] = "Data Deleted sucessfully";
+            }
+            else
+            {
+                TempData["errorMessage"] = "Data is invalid";
+            }
+            return View();
+        }
+
+    }
+}
